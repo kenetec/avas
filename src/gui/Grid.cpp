3fc2daf1e0c8bcb583c71fef7422a0c11b6ac77d
kenetec <klhester3@gmail.com>
2019-12-04 02:46:27 -0600

    Develop Grid Class

--- src/gui/Grid.cpp
+++ src/gui/Grid.cpp
@@ -1,46 +1,84 @@
 #include "Grid.h"
 
-Grid::Grid() {}
+Grid::Grid(int n_rows, int n_cols)
+    : n_rows_(n_rows), n_cols_(n_cols) {}
 
-void Grid::draw(const ImVec2& pos, const ImVec2& size) {
-	
+void Grid::draw(ImDrawList* draw_list, const ImVec2& pos, const ImVec2& size, ImGuiWindowFlags flags) {
+    // set canvas vars
+    canvas_.Update(draw_list, pos, size);
+    vertical_divider_x_offset_ = canvas_.size.x / n_cols_;
+    horizontal_divider_y_offset_ = canvas_.size.y / n_rows_;
+
+	GenerateCellPositions();
+
+    // draw
+    DrawBackground();
+    DrawDividingLines();
 }
 
+void Grid::GenerateCellPositions() {
+    cell_positions_.clear();
+
+    for (int row = 0; row < n_rows_; row++) {
+        std::vector<ImVec2> row_vec;
+
+        for (int col = 0; col < n_cols_; col++) {
+            int x = vertical_divider_x_offset_ * col;
+            int y = horizontal_divider_y_offset_ * row;
+
+            row_vec.push_back(ImVec2(x, y));
+        }
+
+		cell_positions_.push_back(row_vec);
+    }
+}
+
+/*
+Code derived from: https://github.com/ocornut/imgui#integration
+*/
 void Grid::DrawBackground() {
-	// inner background
+    // inner background
     canvas_.draw_list->AddRectFilledMultiColor(
-        canvas_.pos, canvas_.end_pos, IM_COL32(50, 50, 50, 255),
-        IM_COL32(50, 50, 60, 255), IM_COL32(60, 60, 70, 255),
-        IM_COL32(50, 50, 60, 255));
+        canvas_.pos, canvas_.end_pos, background_color_ul_,
+        background_color_ur_, background_color_br_, background_color_bl_);
 
     // border
     canvas_.draw_list->AddRect(canvas_.pos, canvas_.end_pos,
-                               IM_COL32(255, 255, 255, 255));
+                               background_border_color_);
 }
 
 void Grid::DrawDividingLines() {
     // draw vertical
-    for (int i = 0; i < num_of_cols_; i++) {
+    for (int i = 0; i < n_cols_; i++) {
         int x = (vertical_divider_x_offset_ * i) + canvas_.pos.x;
         ImVec2 start_pos(x, canvas_.pos.y);
         ImVec2 end_pos(x, canvas_.end_pos.y);
 
-        canvas_.draw_list->AddLine(start_pos, end_pos,
-                                   IM_COL32(170, 170, 170, 230));
+        canvas_.draw_list->AddLine(start_pos, end_pos, vertical_divider_color_);
     }
 
     // draw horizontal
-    for (int i = 0; i < num_of_rows_; i++) {
+    for (int i = 0; i < n_rows_; i++) {
         int y = (horizontal_divider_y_offset_ * i) + canvas_.pos.y;
         ImVec2 start_pos(canvas_.pos.x, y);
         ImVec2 end_pos(canvas_.end_pos.x, y);
 
         canvas_.draw_list->AddLine(start_pos, end_pos,
-                                   IM_COL32(170, 170, 170, 230));
+                                   horizontal_divider_color_);
     }
 }
 
-void Grid::SetBackgroundColor(const ImU32& color) { background_color_ = color; }
+void Grid::SetBackgroundColor(const ImU32& ul, const ImU32& ur, const ImU32& br,
+                              const ImU32& bl) {
+    background_color_ul_ = ul;
+    background_color_ur_ = ur;
+    background_color_br_ = br;
+    background_color_bl_ = bl;
+}
+
+void Grid::SetBackgroundColor(const ImU32& color) {
+    SetBackgroundColor(color, color, color, color);
+}
 
 void Grid::SetVerticalDividerColor(const ImU32& color) {
     vertical_divider_color_ = color;
@@ -50,10 +88,28 @@ void Grid::SetHorizontalDividerColor(const ImU32& color) {
     horizontal_divider_color_ = color;
 }
 
-void Grid::SetPixelsPerRow(int pixels_per_row) {
-    pixels_per_row_ = pixels_per_row;
+void Grid::SetNumOfRows(int n_rows) { n_rows_ = n_rows; }
+void Grid::SetNumOfCols(int n_cols) { n_cols_ = n_cols; }
+int Grid::GetVerticalDividerXOffset() { return vertical_divider_x_offset_; }
+int Grid::GetHorizontalDividerYOffset() { return horizontal_divider_y_offset_; }
+int Grid::GetNumOfRows() { return n_rows_; }
+int Grid::GetNumOfCols() { return n_cols_; }
+
+ImVec2 Grid::GetCellRelativePosition(int row_index, int col_index) {
+    if ((row_index >= 0 && row_index < n_rows_) &&
+	(col_index >= 0 && col_index < n_cols_)) {
+        return cell_positions_.at(row_index).at(col_index);
+    }
+
+	stringstream ss;
+    ss << "No cell at (" << row_index << ", " << col_index << ")!";
+    throw ss.str();
 }
 
-void Grid::SetPixelsPerCol(int pixels_per_col) {
-    pixels_per_col_ = pixels_per_col;
+ImVec2 Grid::GetCellAbsolutePosition(int row_index, int col_index) {
+    return GetCellRelativePosition(row_index, col_index) + canvas_.pos;
+}
+
+ImVec2 Grid::GetCellSize() {
+    return ImVec2(vertical_divider_x_offset_, horizontal_divider_y_offset_);
 }
--- src/gui/Grid.h
+++ src/gui/Grid.h
@@ -7,30 +7,48 @@
 class Grid {
    private:
     Canvas canvas_;
-    int pixels_per_row_;
-    int pixels_per_col_;
+    int n_cols_ = 0;
+    int n_rows_ = 0;
 
-	int num_of_cols_;
-    int num_of_rows_;
+    std::vector<std::vector<ImVec2>> cell_positions_;
+    int vertical_divider_x_offset_ = 0;
+    int horizontal_divider_y_offset_ = 0;
 
-	int vertical_divider_x_offset_;
-    int horizontal_divider_y_offset_;
+    // background colors //
+    ImU32 background_color_ul_ = IM_COL32(50, 50, 50, 255);
+    ImU32 background_color_ur_ = IM_COL32(50, 50, 60, 255);
+    ImU32 background_color_br_ = IM_COL32(60, 60, 70, 255);
+    ImU32 background_color_bl_ = IM_COL32(50, 50, 60, 255);
+    ImU32 background_border_color_ = IM_COL32(225, 255, 255, 255);
 
-	ImU32 background_color_;
-    ImU32 vertical_divider_color_;
-    ImU32 horizontal_divider_color_;
+    // divider colors //
+    ImU32 vertical_divider_color_ = IM_COL32(170, 170, 170, 230);
+    ImU32 horizontal_divider_color_ = IM_COL32(170, 170, 170, 230);
 
    public:
-    Grid();
-	void draw(const ImVec2& pos, const ImVec2& size);
+    Grid(){};
+    Grid(int n_rows, int n_cols);
+    void draw(ImDrawList* draw_list, const ImVec2& pos, const ImVec2& size,
+              ImGuiWindowFlags flags);
 
-	void SetBackgroundColor(const ImU32& color);
+    void SetBackgroundColor(const ImU32& ul, const ImU32& ur, const ImU32& br,
+                            const ImU32& bl);
+    void SetBackgroundColor(const ImU32& color);
     void SetVerticalDividerColor(const ImU32& color);
     void SetHorizontalDividerColor(const ImU32& color);
-    void SetPixelsPerRow(int pixels_per_row);
-    void SetPixelsPerCol(int pixels_per_cal);
+    void SetNumOfRows(int n_rows);
+    void SetNumOfCols(int n_cols);
 
-	private:
+    int GetVerticalDividerXOffset();
+    int GetHorizontalDividerYOffset();
+    int GetNumOfRows();
+    int GetNumOfCols();
+    ImVec2 GetCellRelativePosition(int row_index, int col_index);
+    ImVec2 GetCellAbsolutePosition(int row_index, int col_index);
+    ImVec2 GetCellSize();
+
+   private:
+    void GenerateCellPositions();
     void DrawBackground();
     void DrawDividingLines();
 };
